36f0627284e646e33e4d9d2fad00116e28631481
027candrafebriyanto <24091397027@mhs.unesa.ac.id>
2025-02-24 21:24:28 +0700

    update

--- python/camera.py
+++ python/camera.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import numpy as np
 import math
 
-# Inisialisasi MediaPipe
+# note:Inisialisasi MediaPipe
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
