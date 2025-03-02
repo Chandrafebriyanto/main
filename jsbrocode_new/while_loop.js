04c2a34804c4c4a7bd8e9cc6807c55dc9f555b8c
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-03-02 13:48:40 +0700

    add file

---
+++ jsbrocode_new/while_loop.js
@@ -0,0 +1,25 @@
+// let num = 0;
+
+//example 1
+// let username = "candra"
+//
+// while(username === ""){
+//     username = window.prompt("Enter your name");
+// }
+//
+// console.log(`hello ${username}`);
+
+//example 2
+num = 0
+while (num > 10){
+    console.log(`hello world`);
+    num++
+}
+
+// //example 3
+// while(num<5){
+//     for(let i=1; i<=5; i++){
+//         console.log(`hello world ${i}`);
+//     }
+//     num += 1
+// }
