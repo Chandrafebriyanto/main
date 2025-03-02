6a84944003b2fc0e70eb018219eb4c54cffbf82c
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-03-02 14:02:50 +0700

    add file

--- jsbrocode_new/while_loop.js
+++ jsbrocode_new/while_loop.js
@@ -10,13 +10,34 @@
 // console.log(`hello ${username}`);
 
 //example 2
-num = 0
-while (num > 10){
-    console.log(`hello world`);
-    num++
+num = 10
+while (num < 10){
+    console.log(`hello world ${num}`);
+    num ++
 }
 
-// //example 3
+for (let i = 0; i < num; i++) {
+    console.log(i)
+}
+
+//example 3
+let loggin = false;
+let username;
+let password;
+
+while (!loggin){
+    username = prompt("Please enter your name");
+    password = prompt("Please enter your password");
+    if (username === "username" || password === "password"){
+        loggin = true;
+        console.log(`welcome back your account ${username}`);
+    }
+    else{
+        console.log("try again")
+    }
+}
+
+// //example 4
 // while(num<5){
 //     for(let i=1; i<=5; i++){
 //         console.log(`hello world ${i}`);
