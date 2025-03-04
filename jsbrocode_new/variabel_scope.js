6aee4399ffee24f8962eb0bc755f7660522d982d
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-03-04 14:33:01 +0700

    modify

--- jsbrocode_new/variabel_scope.js
+++ jsbrocode_new/variabel_scope.js
@@ -10,3 +10,19 @@ function myFunction() {
 }
 
 myFunction();
+
+// tittle: variable scope = where a variable is recognized and accessible (local vs global)            
+
+let x = 3; // global scope
+
+function1();
+
+function function1(){
+    let x = 1; // local scope
+    console.log(x);
+}
+
+function function2(){
+    let x = 2; // local scope
+    console.log(x);
+}
