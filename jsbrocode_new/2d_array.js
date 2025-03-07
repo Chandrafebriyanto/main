1fed8f9d6f03c1a12666b2b559605dcd8f502904
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-03-07 20:18:29 +0700

    modify

--- jsbrocode_new/2d_array.js
+++ jsbrocode_new/2d_array.js
@@ -2,6 +2,12 @@ const matrix = [[1, 2, 3],
                 [4, 5, 6],
                 [7, 8, 9]]
 
+matrix[0][0] = 'X'
+matrix[1][0] = 'O'
+matrix[0][1] = 'X'
+matrix[0][2] = 'O'
+
+
 for(let row of matrix){
     const rowstring = row.join(' ');
     console.log(rowstring)
