c8611a72e9bc6dd29d1d46983e9e04e7d1cb788d
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-03-28 20:49:10 +0700

    commit

--- jsbrocode_new/arrow.function.js
+++ jsbrocode_new/arrow.function.js
@@ -1,10 +1,16 @@
 //arrow function adalah fungsi yang ditulis dengan sintaksis yang lebih ringkas
 //dan lebih mudah dibaca
 
+// arrow deklarasi
+function name3(){
+    console.log('Hello World')
+}
+name3()
+
+//arrow function
 const name1 = () => {
     console.log('Hello World')
 }
-
 name1()
 
 //sama dengan function expression
