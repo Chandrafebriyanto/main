656eee51043284261f0686fbd954521ed15ae96e
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-03-28 20:45:39 +0700

    add

---
+++ jsbrocode_new/arrow.function.js
@@ -0,0 +1,14 @@
+//arrow function adalah fungsi yang ditulis dengan sintaksis yang lebih ringkas
+//dan lebih mudah dibaca
+
+const name1 = () => {
+    console.log('Hello World')
+}
+
+name1()
+
+//sama dengan function expression
+const name2 = function () {
+    console.log('Hello World')
+}
+name2()
