93b8eb0610cc02855daf43f1bfe9f73a9797e6b8
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-03-28 20:52:52 +0700

    modify

--- jsbrocode_new/arrow.function.js
+++ jsbrocode_new/arrow.function.js
@@ -2,19 +2,33 @@
 //dan lebih mudah dibaca
 
 // arrow deklarasi
-function name3(){
-    console.log('Hello World')
+function name3() {
+  console.log("Hello World");
 }
-name3()
+name3();
 
 //arrow function
 const name1 = () => {
-    console.log('Hello World')
-}
-name1()
+  console.log("Hello World");
+};
+name1();
 
 //sama dengan function expression
 const name2 = function () {
-    console.log('Hello World')
-}
-name2()
+  console.log("Hello World");
+};
+name2();
+
+setTimeout(() => {
+  console.log("Hello");
+  console.log("Goodbye");
+}, 3000);
+//setTimeout adalah fungsi yang digunakan untuk menunda eksekusi kode selama waktu tertentu
+
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const oddNums = numbers.filter((element) => element % 2 !== 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
+
+console.log(total);
