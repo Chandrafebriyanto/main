e1a54e0739eb6e80217bc89453d13518886ad404
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-05-22 11:40:19 +0700

    commit

--- tugas/project.py
+++ tugas/project.py
@@ -1,4 +1,5 @@
 import heapq
+import itertools
 
 # note: membuat variabel negara
 ind  = "indonesia"
@@ -15,16 +16,16 @@ vnm  = "vietnam"
 
 
 graph = {
-    ind: {sgp: 863, brn:1300, mal:1369, mal:1369},
-    brn: {ind: 100, sgp:1301, khm:1414, phl:1259},
-    khm: {mal: 1029, vnm:421, brn:1505, tha:527, sgp:1295, phl: 1942},
-    lao: {myan: 686, vnm: 837, tha: 425},
-    mal: {ind:1369, sgp: 411, khm: 1071},
+    ind : {sgp: 863, brn:1300, mal:1369, mal:1369},
+    brn : {ind: 100, sgp:1301, khm:1414, phl:1259},
+    khm : {mal: 1029, vnm:421, brn:1505, tha:527, sgp:1295, phl: 1942},
+    lao : {myan: 686, vnm: 837, tha: 425},
+    mal : {ind:1369, sgp: 411, khm: 1071},
     myan: {lao: 686, tha: 857},
-    phl: {brn: 1259, khm: 1942, vnm: 1625},
-    sgp: {ind:863, mal:411, brn: 1301, khm: 1295},
-    tha: {myan: 857, lao: 425, khm: 527, vnm: 730},
-    vnm: {khm: 421, lao: 837, tha: 730, phl: 1625}
+    phl : {brn: 1259, khm: 1942, vnm: 1625},
+    sgp : {ind:863, mal:411, brn: 1301, khm: 1295},
+    tha : {myan: 857, lao: 425, khm: 527, vnm: 730},
+    vnm : {khm: 421, lao: 837, tha: 730, phl: 1625}
 }
 
 def dijkstra(graph, start, end):
@@ -44,10 +45,43 @@ def dijkstra(graph, start, end):
                 heapq.heappush(queue, (cost + weight, neighbor, path))
     return float("inf"), []
 
+def tsp(graph, start):
+    cities = list(graph.keys())
+    cities.remove(start)
+    min_path = []
+    min_cost = float("inf")
+    
+    for perm in itertools.permutations(cities):
+        cost = 0
+        route = [start] + list(perm)
+        valid = True
+        for i in range(len(route) - 1):
+            src, dst = route[i], route[i + 1]
+            if dst in graph[src]:
+                cost += graph[src][dst]
+            else:
+                valid = False
+                break
+        if valid and cost < min_cost:
+            min_cost = cost
+            min_path = route
+            
+    return min_cost, min_path
+
+
 awal = input("Masukkan kota awal: ").lower()
 akhir = input("Masukkan kota akhir: ").lower()
 
+# tittle: algoritma dijkstra
 total_cost, route = dijkstra(graph, awal, akhir)
 
+print("----------algoritma dijkstra----------")
 print(f"kota yang dilalui dari {awal} ke {akhir}: {' -> '.join(route)}")
 print(f"Total jarak tempu dari {awal} ke {akhir}: {total_cost} km")
+
+# tittle: algoritma tsp
+
+total_cost, route = tsp(graph, awal)
+print("\n------------algoritma tsp-------------")
+print(f"rute terpendek dari {awal} ke semua negara: {' -> '.join(route)}")
+print(f"Total jarak tempu dari {awal} ke semua negara: {total_cost} km")
