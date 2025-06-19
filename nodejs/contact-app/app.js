884bf5982604e197153563ebc9a42b95f5b358ce
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-06-19 20:28:26 +0700

    commit

--- nodejs/contact-app/app.js
+++ nodejs/contact-app/app.js
@@ -1,60 +1,76 @@
 const yargs = require("yargs");
-const { simpanContact } = require("./contact.js");
+const { simpanContact, listcontact, detailContact, removeContact } = require("./contact.js");
 
+yargs
+  .command({
+    command: "add",
+    describe: "Menambahkan contact baru",
+    builder: {
+      nama: {
+        describe: "Nama lengkap",
+        demandOption: true, // Harus diisi
+        type: "string",
+      },
+      email: {
+        describe: "Email",
+        demandOption: false, // Tidak wajib diisi
+        type: "string",
+      },
+      nomer: {
+        describe: "Nomer telepon",
+        demandOption: true,
+        type: "string",
+      },
+    },
+
+    handler(argv) {
+      simpanContact(argv.nama, argv.email, argv.nomer);
+    },
+  })
+  .demandCommand();
+
+// tittle: menampilkan daftar kontak
 yargs.command({
-  command: "add",
-  describe: "Menambahkan contact baru",
+  command: "list",
+  describe: "Menampilkan daftar kontak",
+  handler() {
+    listcontact();
+  },
+});
+
+// tittle: menmapilkan detail kontak
+yargs.command({
+  command: "detail",
+  describe: "Menampilkan detail kontak",
   builder: {
     nama: {
       describe: "Nama lengkap",
-      demandOption: true, // Harus diisi
-      type: "string",
-    },
-    email: {
-      describe: "Email",
-      demandOption: false, // Tidak wajib diisi
-      type: "string",
-    },
-    nomer: {
-      describe: "Nomer telepon",
       demandOption: true,
       type: "string",
     },
   },
-
   handler(argv) {
-    simpanContact(argv.nama, argv.email, argv.nomer);
-  }
+    detailContact(argv.nama);
+  },
+});
+
+// tittle: menghapus kontak
+yargs.command({
+  command: "remove",
+  describe: "Menghapus kontak",
+  builder: {
+    nama: {
+      describe: "Nama lengkap",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    removeContact(argv.nama);
+  },
 });
 
 yargs.parse();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const {tulisPertanyaan, simpanContact} = require("./contact.js");
 
--- nodejs/contact-app/contact.js
+++ nodejs/contact-app/contact.js
@@ -1,7 +1,7 @@
 // tittle: core module
 const fs = require("fs");
 const readline = require("readline");
-var validator = require('validator');
+var validator = require("validator");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -29,11 +29,18 @@ const tulisPertanyaan = (pertanyaan) => {
   });
 };
 
+const loadcontact = () => {
+  const filleBuffer = fs.readFileSync("data/contact.json", "Utf-8");
+  const contacts = JSON.parse(filleBuffer);
+  return contacts;
+};
+
 // tittle: simpan contact
 const simpanContact = (nama, email, nomer) => {
   const contact = { nama, email, nomer };
-  const filleBuffer = fs.readFileSync("data/contact.json", "Utf-8");
-  const contacts = JSON.parse(filleBuffer);
+  // const filleBuffer = fs.readFileSync("data/contact.json", "Utf-8");
+  // const contacts = JSON.parse(filleBuffer);
+  const contacts = loadcontact();
 
   // tittle: cek duplikat
   const duplikat = contacts.find((contact) => contact.nama === nama);
@@ -57,11 +64,69 @@ const simpanContact = (nama, email, nomer) => {
     }
   }
 
+  // tittle: cek nomer
+  const nomerExists = contacts.find((contact) => contact.nomer === nomer);
+  if (!validator.isMobilePhone(nomer, "id-ID")) {
+    console.log("nomer tidak valid");
+    rl.close();
+    return false;
+  } else {
+    if (nomerExists) {
+      console.log("nomer sudah terdaftar, silahkan gunakan nomer lain");
+      rl.close();
+      return false;
+    }
+  }
 
   contacts.push(contact);
   fs.writeFileSync("data/contact.json", JSON.stringify(contacts));
   console.log("terima kasih udah menambahkan data");
-  rl.close()
+  rl.close();
 };
 
-module.exports = {tulisPertanyaan, simpanContact} 
+// tittle: menampilkan daftar kontak
+const listcontact = () => {
+  const contacts = loadcontact();
+  console.log("Daftar Kontak:");
+  contacts.forEach((contacts, i) => {
+    console.log(`${i + 1}. ${contacts.nama} -  ${contacts.nomer}`);
+  });
+  rl.close();
+};
+
+// tittle: menampilkan detail kontak
+const detailContact = (nama) => {
+  const contacts = loadcontact();
+  const contact = contacts.find(
+    (c) => c.nama.toLowerCase() === nama.toLowerCase()
+  );
+  if (!contact) {
+    console.log(`Kontak dengan nama ${nama} tidak ditemukan.`);
+    rl.close();
+    return;
+  }
+  console.log(`Nama: ${contact.nama}`);
+  console.log(`Nomer: ${contact.nomer}`);
+  if (contact.email) {
+    console.log(`Email: ${contact.email}`);
+  }
+  rl.close();
+};
+
+// tittle: menghapus kontak
+const removeContact = (nama) => {
+  const contacts = loadcontact();
+  const newContacts = contacts.filter(
+    (contact) => contact.nama.toLowerCase() !== nama.toLowerCase()
+  );
+  if (contacts.length === newContacts.length) {
+    console.log(`Kontak dengan nama ${nama} tidak ditemukan.`);
+    rl.close();
+    return;
+  }
+  fs.writeFileSync("data/contact.json", JSON.stringify(newContacts));
+  console.log(`Kontak dengan nama ${nama} telah dihapus.`);
+  rl.close();
+}
+
+module.exports = { tulisPertanyaan, simpanContact, detailContact, listcontact, removeContact };
