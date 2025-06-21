61a1c639bdb008a9fe8869670f28e34a67fa32e1
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-06-21 21:03:08 +0700

    commit

--- nodejs/13-nodejs-web-server/app.js
+++ nodejs/13-nodejs-web-server/app.js
@@ -1,48 +1,83 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const morgan = require('morgan');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const { loadcontact, findContact } = require("./utils/contact.js");
+
+const app = express();
+const port = 3000;
 
 // note: gunakan ejs
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+app.use(expressLayouts); // gunakan express-ejs-layouts
+app.use(express.static("public")); // gunakan folder public untuk static files
+// app.use(morgan('tiny')) // gunakan morgan untuk logging
 
-app.get('/', (req, res) => {
+// Middleware untuk parsing data dari form
+// app.use((req, res, next) => {
+//   console.log('Time:', Date.now())
+//   next()
+// });
+
+app.get("/", (req, res) => {
   // res.sendFile(__dirname + '/index.html')
   const mahasiswa = [
     {
-      nama: 'Candra',
-      email: 'candra@gmail.com'
+      nama: "Candra",
+      email: "candra@gmail.com",
     },
     {
-      nama: 'John',
-      email: 'John@gmail.com'
+      nama: "John",
+      email: "John@gmail.com",
     },
     {
-      nama: 'Andre',
-      email: 'andre@gmail.com'
+      nama: "Andre",
+      email: "andre@gmail.com",
     },
-  ]
-  res.render('index', { nama: 'John Doe', mahasiswa, title: 'Home Page' })
-})
+  ];
+  res.render("index", {
+    layout: "layouts/main-layout",
+    nama: "John Doe",
+    mahasiswa,
+    title: "Home Page",
+  });
+});
 
-app.get('/about', (req, res) => {
+app.get("/about", (req, res) => {
   // res.sendFile(__dirname + '/tentang.html')
-  res.render('tentang', { title: 'About Page' })
-})
+  res.render("tentang", {
+    title: "About Page",
+    layout: "layouts/main-layout",
+  });
+});
 
-app.get('/contact', (req, res) => {
+app.get("/contacts", (req, res) => {
   // res.sendFile(__dirname + '/kontak.html')
-  res.render('kontak', { title: 'Contact Page' })
-})
+  const contacts = loadcontact();
+  res.render("kontak", {
+    title: "Contact Page",
+    layout: "layouts/main-layout",
+    contacts,
+  });
+});
 
-app.get('/products/:id', (req, res) => {
-    res.send(`Produk dengan ID: ${req.params.id}`)
-})
+app.get("/contact/:name", (req, res) => {
+  // res.sendFile(__dirname + '/kontak.html')
+  const contact = findContact(req.params.name);
+  res.render("detail", {
+    title: "detail Contact Page",
+    layout: "layouts/main-layout",
+    contact
+  });
+});
+
+app.get("/products/:id", (req, res) => {
+  res.send(`Produk dengan ID: ${req.params.id}`);
+});
 
 app.use((req, res, next) => {
-  res.status(404).send('Halaman tidak ditemukan')
-})
+  res.status(404).send("404 Halaman tidak ditemukan");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ nodejs/13-nodejs-web-server/utils/contact.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const readline = require("readline");
+// var validator = require("validator");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// tittle: membuat directory jika belum ada directory
+const dirpath = "./data";
+if (!fs.existsSync(dirpath)) {
+  fs.mkdirSync(dirpath);
+}
+
+// tittle: membuat file jika belum ada file
+const datapath = "./data/contact.json";
+if (!fs.existsSync(datapath)) {
+  fs.writeFileSync(datapath, "[]", "utf-8");
+}
+
+// tittle: membuat pertanyaan
+const tulisPertanyaan = (pertanyaan) => {
+  return new Promise((resolve, reject) => {
+    rl.question(pertanyaan, (tanya) => {
+      resolve(tanya);
+    });
+  });
+};
+
+const loadcontact = () => {
+  const filleBuffer = fs.readFileSync("data/contact.json", "Utf-8");
+  const contacts = JSON.parse(filleBuffer);
+  return contacts;
+};
+
+const findContact = (name) => {
+  const contacts = loadcontact();
+  const contact = contacts.find((contact) => contact.name === name);
+  return contact;
+}
+
+module.exports = {loadcontact, findContact};
