40ab7f085bd7fb51a4128e4f8d427c310bae2211
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-07-30 10:32:02 +0700

    commit

---
+++ set_mathematical_operations.py
@@ -0,0 +1,7 @@
+
+
+region1 = {"gold coin", "ruby", "emerald", "pearl"}
+region2 = {"ruby", "emerald", "sapphire"}
+region3 = {"emerald", "diamond", "sapphire"}
+
+print((region1 | region2) ^ (region3 | region2))
