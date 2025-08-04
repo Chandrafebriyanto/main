411ad12ae62eb1144d0b222c75d1f9fde3442a97
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-08-04 07:49:14 +0700

    commit

--- python/Coddy/Basic List Comprehensions/syntax.py
+++ python/Coddy/Basic List Comprehensions/syntax.py
@@ -6,23 +6,23 @@ def double_numbers(numbers):
 print(double_numbers([1, 2, 3, 4, 5]))
 # Output: [2, 4, 6, 8, 10]
 
-# contoh 4:
+# contoh 2:
 numbers = [1, 2, 3, 4, 5]
 squares = [n * n for n in numbers]
 print(squares)
 # Output: [1, 4, 9, 16, 25]
 
-# contoh 5:
+# contoh 3:
 cubes = [x**3 for x in range(1, 6)]
 print(cubes)
 # Output: [1, 8, 27, 64, 125]
 
-# contoh 6:
-chars = [char for char in "hello"]
-print(chars)
-# Output: ['h', 'e', 'l', 'l', 'o']
-
-# contoh 7:
+# contoh 4:
 evens = [x for x in range(2, 11, 2)]
 print(evens)
 # Output: [2, 4, 6, 8, 10]
+
+# contoh 5:
+chars = [char for char in "hello"]
+print(chars)
+# Output: ['h', 'e', 'l', 'l', 'o']
---
+++ python/Coddy/enumerate.py
@@ -0,0 +1,3 @@
+array = ["salak", "apel", "manggis"]
+for i, value in enumerate(array):
+    print(f"{i+1}. {value}")
