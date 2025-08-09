d967e840a6a228f7f950f251ac98738f7da62f50
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-08-09 18:38:37 +0700

    commit

--- js-dasar/number.js
+++ js-dasar/number.js
@@ -7,3 +7,13 @@ console.log(typeof number)
 console.log(typeof input)
 
 //example 2
+console.log(Number("salah"))
+
+//example 3
+console.log(Number.isInteger(number)) //mengecek apakah data ber-type data integer
+console.log(Number.isNaN(number)) //mengecek apakah data berisi NaN (Not a Number)
+console.log(Number.isFinite(number)) //memeriksa apakah suatu nilai adalah angka yang terbatas (finite)
+
+// example 4
+console.log(number.toString(2)) //mengubah data menjadi format binary
+console.log(number.toLocaleString("id-ID")) //mengubah data menjadi format penulisan indonesia
