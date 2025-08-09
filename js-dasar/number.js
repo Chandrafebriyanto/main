45773f8ad07a9cdd6756bb6cf016e0d164aa3968
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-08-09 18:14:54 +0700

    commit

--- js-dasar/number.js
+++ js-dasar/number.js
@@ -1,4 +1,9 @@
+// example 1
 const input = "12345"
-const number = number(input)
+const number = Number(input)
 
 console.log(number)
+console.log(typeof number)
+console.log(typeof input)
+
+//example 2
