c526e0933395dff87f5ba04956db4bbe2a09669d
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-08-10 17:13:43 +0700

    commit

---
+++ js-dasar/string.js
@@ -0,0 +1,8 @@
+const name = "Alexander Kurniawan"
+console.log(name.length)
+console.log(name.toLowerCase())
+console.log(name.toUpperCase())
+console.log(name.split(" "))
+
+const input = "       Input data         "
+console.log(input.trim().toUpperCase())
