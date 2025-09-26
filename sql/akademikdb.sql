768048c29eee171dc1af7cb2cc268f2e2e20ea3d
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-09-26 10:35:30 +0700

    commit

--- sql/akademikdb.sql
+++ sql/akademikdb.sql
@@ -44,194 +44,58 @@ CREATE TABLE Nilai (
 );
 
 -- tittle: INSERT
-INSERT INTO
-    Mahasiswa (
-        MahasiswaID,
-        Nama,
-        Alamat,
-        TanggalLahir,
-        NoHP
-    )
-VALUES (
-        1,
-        'Ali Fauzan',
-        'Surabaya',
-        '2000-01-15',
-        '081234567890'
-    ),
-    (
-        2,
-        'Budi Santoso',
-        'Malang',
-        '1999-05-21',
-        '082345678901'
-    ),
-    (
-        3,
-        'Citra Lestari',
-        'Sidoarjo',
-        '2001-03-10',
-        '083456789012'
-    ),
-    (
-        4,
-        'Deni Kurniawan',
-        'Gresik',
-        '2000-07-25',
-        '084567890123'
-    ),
-    (
-        5,
-        'Eka Putri',
-        'Lamongan',
-        '2001-09-18',
-        '085678901234'
-    );
+INSERT INTO Mahasiswa (MahasiswaID, Nama, Alamat, TanggalLahir, NoHP)
+VALUES  (1, 'Ali Fauzan', 'Surabaya', '2000-01-15', '081234567890'),
+        (2, 'Budi Santoso', 'Malang', '1999-05-21', '082345678901'),
+        (3, 'Citra Lestari', 'Sidoarjo', '2001-03-10', '083456789012'),
+        (4, 'Deni Kurniawan', 'Gresik', '2000-07-25', '084567890123'),
+        (5,'Eka Putri','Lamongan','2001-09-18','085678901234');
 
-INSERT INTO
-    Dosen (DosenID, Nama, Email, NoHP)
-VALUES (
-        10,
-        'Dr. Rina Widya',
-        'rina.widya@univ.ac.id',
-        '081122334455'
-    ),
-    (
-        11,
-        'Dr. Yusuf Malik',
-        'yusuf.malik@univ.ac.id',
-        '082233445566'
-    ),
-    (
-        12,
-        'Dr. Sinta Dewi',
-        'sinta.dewi@univ.ac.id',
-        '083344556677'
-    ),
-    (
-        13,
-        'Dr. Arif Hidayat',
-        'arif.hidayat@univ.ac.id',
-        '084455667788'
-    ),
-    (
-        14,
-        'Dr. Lestari Nur',
-        'lestari.nur@univ.ac.id',
-        '085566778899'
-    );
+INSERT INTO Dosen (DosenID, Nama, Email, NoHP)
+VALUES  (10, 'Dr. Rina Widya', 'rina.widya@univ.ac.id', '081122334455'),
+        (11, 'Dr. Yusuf Malik', 'yusuf.malik@univ.ac.id', '082233445566'),
+        (12, 'Dr. Sinta Dewi', 'sinta.dewi@univ.ac.id', '083344556677'),
+        (13, 'Dr. Arif Hidayat', 'arif.hidayat@univ.ac.id', '084455667788'),
+        (14, 'Dr. Lestari Nur', 'lestari.nur@univ.ac.id', '085566778899'),
+        (15, 'Dr. Rahmat Hidayah', 'rahmat.hidaya@univ.ac.id', '081299988877');
 
-INSERT INTO
-    MataKuliah (
-        MataKuliahID,
-        NamaMK,
-        SKS,
-        Semester
-    )
-VALUES (100, 'Basis Data', 3, 4),
-    (
-        101,
-        'Algoritma dan Pemrograman',
-        3,
-        2
-    ),
-    (
-        102,
-        'Matematika Diskrit',
-        2,
-        2
-    ),
-    (103, 'Sistem Operasi', 3, 3),
-    (
-        104,
-        'Jaringan Komputer',
-        2,
-        4
-    );
+INSERT INTO MataKuliah (MataKuliahID, NamaMK, SKS, Semester)
+VALUES  (100, 'Basis Data', 3, 4),
+        (101, 'Algoritma dan Pemrograman', 3, 2),
+        (102, 'Matematika Diskrit', 2, 2),
+        (103, 'Sistem Operasi', 3, 3),
+        (104, 'Jaringan Komputer', 2, 4),
+        (105, 'Kecerdasan Buatan', 3, 5);
 
-INSERT INTO
-    Kelas (
-        KelasID,
-        MataKuliahID,
-        DosenID,
-        TahunAkademik
-    )
-VALUES (201, 100, 10, '2024/2025'),
-    (202, 101, 11, '2024/2025'),
-    (203, 102, 12, '2024/2025'),
-    (204, 103, 13, '2024/2025'),
-    (205, 104, 14, '2024/2025');
+INSERT INTO Kelas (KelasID, MataKuliahID, DosenID, TahunAkademik)
+VALUES  (201, 100, 10, '2024/2025'),
+        (202, 101, 11, '2024/2025'),
+        (203, 102, 12, '2024/2025'),
+        (204, 103, 13, '2024/2025'),
+        (205, 104, 14, '2024/2025');
 
-INSERT INTO
-    Nilai (
-        NilaiID,
-        MahasiswaID,
-        KelasID,
-        Nilai
-    )
-VALUES (301, 1, 201, 85.5),
-    (302, 2, 202, 90.0),
-    (303, 3, 203, 78.0),
-    (304, 4, 204, 88.0),
-    (305, 5, 205, 92.5);
+INSERT INTO Nilai (NilaiID, MahasiswaID, KelasID, Nilai)
+VALUES  (301, 1, 201, 85.5),
+        (302, 2, 202, 90.0),
+        (303, 3, 203, 78.0),
+        (304, 4, 204, 88.0),
+        (305, 5, 205, 92.5);
 
 -- tittle: MANIPULASI DATA
 -- tittle:INSERT
-INSERT INTO
-    mahasiswa (
-        MahasiswaID,
-        Nama,
-        Alamat,
-        TanggalLahir,
-        NoHP
-    )
-VALUES (
-        6,
-        'Gita Pratiwi',
-        'Semarang',
-        '2002-12-15',
-        '085755553333'
-    );
+INSERT INTO mahasiswa (MahasiswaID, Nama, Alamat, TanggalLahir, NoHP)
+VALUES (6,'Gita Pratiwi','Semarang','2002-12-15','085755553333');
 
-INSERT INTO
-    dosen (DosenID, Nama, Email, NoHP)
-VALUES (
-        15,
-        'Dr. Rahmat Hidayah',
-        'rahmat.hidaya@univ.ac.id',
-        '081299988877'
-    );
+INSERT INTO dosen (DosenID, Nama, Email, NoHP)
+VALUES (15,'Dr. Rahmat Hidayah','rahmat.hidaya@univ.ac.id','081299988877');
 
-INSERT INTO
-    matakuliah (
-        MataKuliahID,
-        NamaMK,
-        SKS,
-        Semester
-    )
-VALUES (
-        105,
-        'Kecerdasan Buatan',
-        3,
-        5
-    );
+INSERT INTO matakuliah (MataKuliahID, NamaMK, SKS, Semester)
+VALUES (105, 'Kecerdasan Buatan', 3, 5);
 
-INSERT INTO
-    kelas (
-        KelasID,
-        MataKuliahID,
-        DosenID,
-        TahunAkademik
-    )
+INSERT INTO kelas (KelasID, MataKuliahID, DosenID, TahunAkademik)
 VALUES (206, 105, 15, '2024/2025');
 
-INSERT INTO
-    nilai (
-        NilaiID,
-        MahasiswaID,
-        KelasID,
-        Nilai
-    )
+INSERT INTO nilai (NilaiID, MahasiswaID, KelasID, Nilai)
 VALUES (306, 6, 206, 91.0);
 
 -- tittle: SELECT
