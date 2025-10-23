cf5eb64b4fc53bcb80f1b1f809f2269c43b00fa6
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-10-23 21:20:25 +0700

    commit

---
+++ learn-react/my-react-app/src/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to My React App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
