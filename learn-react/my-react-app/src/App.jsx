eff79b7a9d561d5dac97f9315f9a3454a739ceb9
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-10-24 21:00:26 +0700

    commit

--- learn-react/my-react-app/src/App.jsx
+++ learn-react/my-react-app/src/App.jsx
@@ -1,13 +1,11 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
+import Card from "./Card";
 
 function App() {
   return (
     <>
-      <Header />
-      <Food />
-      <Footer />
+      <Card />
+      <Card />
+      <Card />
     </>
   );
 }
---
+++ learn-react/my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="foto profil" className='card-image' />
+            <h2 className='card-tittle'>Candra Febriyanto</h2>
+            <p className='card-text'>I am student in college and software engineer freelance</p>
+        </div>
+    )
+}
+
+export default Card;
