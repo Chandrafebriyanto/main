0bd94b39952ff43d4526f14b07c2eca00bd92ab5
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-10-25 20:49:39 +0700

    commit

--- learn-react/my-react-app/src/UserGreeting.jsx
+++ learn-react/my-react-app/src/UserGreeting.jsx
@@ -1,7 +1,17 @@
+import PropTypes from 'prop-types';
 
+function UserGreeting({ isLoggedIn = false, username = 'Guest' }) {
 
-function UserGreeting(props) {
-    return(props.isLoggedIn ? <h1 className="welcome-message">Welcome back, {props.username}!</h1> : <h1 className="login-prompt">Please sign up.</h1>);
+    const welcomeMessage = <h1 className="welcome-message">Welcome back, {username}!</h1>;
+    const loginPrompt = <h1 className="login-prompt">Please sign up.</h1>;
+
+    return(isLoggedIn ? welcomeMessage : loginPrompt);
 }
+
+UserGreeting.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  username: PropTypes.string,
+};
+
 
 export default UserGreeting;
