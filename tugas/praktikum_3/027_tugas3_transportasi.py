986b37d185708252d5b1a3cdd78d73b6c8fd2cb3
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-10-29 21:05:41 +0700

    commit

---
+++ tugas/praktikum_3/027_tugas3_transportasi.py
@@ -0,0 +1,19 @@
+class Transportasi:
+    def bergerak(self):
+        print("Bergerak di...")
+
+class Mobil(Transportasi):
+    def bergerak(Self):
+        print("Mobil bergerak di jalan raya")
+        
+class Kapal(Transportasi):
+    def bergerak(Self):
+        print("Kapal bergerak di laut")
+        
+class Pesawat(Transportasi):
+    def bergerak(Self):
+        print("Pesawat bergerak di udara")
+
+daftar_kendaraan = [Mobil(), Kapal(), Pesawat()]
+for kendaraan in daftar_kendaraan:
+    kendaraan.bergerak() 
